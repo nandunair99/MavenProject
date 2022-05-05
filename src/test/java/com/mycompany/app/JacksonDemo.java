ae52a637d05933affc5570ad39d06441fa5788f1
nun2 <nun2@gmail.com>
2022-05-05 10:57:16 +0530

    Added mapper object

--- src/test/java/com/mycompany/app/JacksonDemo.java
+++ src/test/java/com/mycompany/app/JacksonDemo.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonDemo {
@@ -7,5 +13,6 @@ public class JacksonDemo {
 	public static void main(String args[])
 	{
 		ObjectMapper mapper=new ObjectMapper();
+		
 	}
 }
