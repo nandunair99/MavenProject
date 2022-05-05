acc11f8222374b87089f882b3e260e53636e5699
nun2 <nun2@gmail.com>
2022-05-05 10:40:00 +0530

    Added Jackson dependencies in jackson branch

---
+++ src/test/java/com/mycompany/app/JacksonDemo.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonDemo {
+
+	public static void main(String args[])
+	{
+		ObjectMapper mapper=new ObjectMapper();
+	}
+}
